d14be1b9fb642fbe1a1b37caaadb104431f148ec
oyevamos <deadhead2520@gmail.com>
2024-03-20 00:04:06 +0300

    Исправлено замечание про поиск свободного порта, написаны комменты в main.go

--- main.go
+++ main.go
@@ -7,24 +7,15 @@ import (
 	"net/http"
 )
 
-// import (
-// 	"net/http"
-// )
-
-// func main() {
-// 	http.HandleFunc("/hello", helloHandler)
-// 	setupWeatherRoutes()
-// 	http.ListenAndServe(":8080", nil)
-// }
-
 func main() {
 	apiConfig, err := loadApiConfig("C:/Users/User/Desktop/FILES/go/weather_tracker/weather_tracker/.apiConfig")
 	if err != nil {
 		log.Fatal("Ошибка при загрузке конфигурации: ", err)
 	}
+
 	port := apiConfig.Port
 	if port == "" {
-		port = "0"
+		port = "0" // показывает что я не хочу конерктизироваить порт, его выберет система.
 	}
 
 	listener, err := net.Listen("tcp", ":"+port)
@@ -33,10 +24,10 @@ func main() {
 	}
 	defer listener.Close()
 
-	fmt.Printf("Сервер запущен на порту %d\n", listener.Addr().(*net.TCPAddr).Port)
+	fmt.Printf("Сервер запущен на порту %d\n", listener.Addr().(*net.TCPAddr).Port) //заклинание для нахождения свободного порта, взято отсюда https://stackoverflow.com/questions/43424787/how-to-use-next-available-port-in-http-listenandserve
 
 	setupWeatherRoutes()
 	http.HandleFunc("/hello", helloHandler)
 
-	http.Serve(listener, nil)
+	http.Serve(listener, nil) // Запускаем сервер с использованием созданного listener
 }
